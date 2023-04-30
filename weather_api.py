60bf2b42cfd13047dd70a5f61d0f46cb2b66d469
maroua <maroua.boudoukha@efrei.net>
2023-04-30 23:11:55 +0200

    TP2

--- weather_api.py
+++ weather_api.py
@@ -1,10 +1,16 @@
 import os
 import requests
 from dotenv import load_dotenv
+from flask import Flask
 
+app = Flask(__name__)
 load_dotenv()
 
-def get_current_weather(latitude, longitude, api_key):
+@app.route('/')
+def get_weather():
+    latitude = os.environ.get('LATITUDE')
+    longitude = os.environ.get('LONGITUDE')
+    api_key = os.environ.get('OPENWEATHER_API_KEY')
     url = f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric'
     response = requests.get(url)
     response_json = response.json()
@@ -13,10 +19,5 @@ def get_current_weather(latitude, longitude, api_key):
     feels_like = response_json['main']['feels_like']
     return f'Current weather: {weather_description}, Temperature: {temperature}°C, Feels like: {feels_like}°C'
 
-# Retrieve latitude, longitude, and API key from environment variables
-latitude = os.environ.get('LATITUDE')
-longitude = os.environ.get('LONGITUDE')
-api_key = os.environ.get('OPENWEATHER_API_KEY')
-
-# Call the function to get the current weather information
-print(get_current_weather(latitude, longitude, api_key))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081)
